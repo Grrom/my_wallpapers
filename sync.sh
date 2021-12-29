ce89e50afe351b740cd830ac5627400d90d1e4b7
Jerome <=>
2021-12-29 20:38:22 +0800

    minimize sync script

--- sync.sh
+++ sync.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo "↓   Syncing with the repository...   ↓"
-status=$(git pull)
-echo $status
-if [[ "$status" == "Already up to date." ]]; then
-  echo "nothing to update"
-else
-  ./copy
-fi
+git pull
+./copy
+
